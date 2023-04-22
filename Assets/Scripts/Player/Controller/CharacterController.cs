eaca615a3f91a96056f74dcc216f7cdb797d20e3
Berk Örenbaşı <183301060@ogr.selcuk.edu.tr>
2023-04-22 22:11:09 +0300

    Added Character Movement System v0.1

--- Assets/Scripts/Player/Controller/CharacterController.cs
+++ Assets/Scripts/Player/Controller/CharacterController.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerInput))]
 public class CharacterController : MonoBehaviour
 {
     #region Variables
     private Rigidbody2D rb;
     private PlayerInputAction playerInput;
-    private Vector2 moveInput;
-    
+    private PlayerInput playerInputComponent;
+
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 200f;
+    [SerializeField] private SpriteRenderer bodySpriteRenderer;
+
+    [Header("Scale Effect")]
+    [SerializeField] private float scaleSpeed = 1.0f;
+    [SerializeField] private float changeSize = 0.1f;
+    private Vector3 currentScale = new Vector3();
+    private Vector3 constantScale;
+    private float minScale;
+    private float maxScale;
+    private int scaleDirection = 1;
+
+    private bool isMove = false;
     #endregion
 
     #region Unity Methods
@@ -18,39 +31,90 @@ public class CharacterController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    #endregion
 
-    #region Input Methods
     private void OnEnable()
     {
-        if (playerInput == null)
-        {
-            playerInput = new PlayerInputAction();
-            playerInput.Player.Move.performed += Move_performed;
-            playerInput.Player.Move.canceled += Move_canceled;
-        }
-
-        playerInput.Enable();
+        InitializePlayerInput();
+        InitializedScale();
     }
 
     private void OnDisable()
     {
         playerInput.Disable();
     }
-    
-    private void OnMove(InputValue value)
+
+    private void Update()
     {
-        rb.velocity = value.Get<Vector2>() * moveSpeed * Time.fixedDeltaTime;
+        if(isMove) UpdateScale();
+        else ResetScale();
+    }
+    #endregion
+
+    #region Initialization Methods
+    private void InitializePlayerInput()
+    {
+        playerInputComponent = GetComponent<PlayerInput>();
+        playerInput = new PlayerInputAction();
+
+        playerInput.Enable();
+        if (playerInputComponent.actions == null)
+        {
+            playerInputComponent.actions = playerInput.asset;
+        }
+
+        playerInput.Player.Move.performed += Move_performed;
+        playerInput.Player.Move.canceled += Move_canceled;
     }
 
+    private void InitializedScale()
+    {
+        constantScale = bodySpriteRenderer.transform.localScale;
+        minScale = constantScale.y - changeSize;
+        maxScale = constantScale.y + changeSize;
+    }
+    #endregion
+
+    #region Input Methods
     private void Move_performed(InputAction.CallbackContext obj)
     {
-        moveInput = obj.ReadValue<Vector2>();
+        rb.velocity = obj.ReadValue<Vector2>() * moveSpeed * Time.fixedDeltaTime;
+
+        if (obj.ReadValue<Vector2>().x > 0)
+        {
+            bodySpriteRenderer.flipX = false;
+        }
+        else if (obj.ReadValue<Vector2>().x < 0)
+        {
+            bodySpriteRenderer.flipX = true;
+        }
+
+        isMove = true;
     }
 
     private void Move_canceled(InputAction.CallbackContext obj)
     {
-        moveInput = Vector2.zero;
+        rb.velocity = Vector2.zero;
+        isMove = false;
+    }
+    #endregion
+
+    #region Scale Effect Methods
+    private void UpdateScale()
+    {
+        currentScale.y += scaleDirection * scaleSpeed * Time.deltaTime;
+        currentScale.y = Mathf.Clamp(currentScale.y, minScale, maxScale);
+        bodySpriteRenderer.transform.localScale = new Vector3(constantScale.x, currentScale.y, constantScale.z);
+
+        if (currentScale.y <= minScale || currentScale.y >= maxScale)
+        {
+            scaleDirection *= -1;
+        }
+    }
+
+    private void ResetScale()
+    {
+        currentScale = constantScale;
+        bodySpriteRenderer.transform.localScale = new Vector3(constantScale.x, currentScale.y, constantScale.z);
     }
     #endregion
 }
